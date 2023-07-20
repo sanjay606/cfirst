d8c8df78db56069122dc42e120af24d2ec268e3e
sanjay <sanjaylashkarmaa1baba1@gmail.com>
2023-07-20 13:44:14 +0530

    new armstrong

---
+++ armstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num=153,rem,sum=0;
+    int temp;
+    while (num>0)
+    {
+        rem=num%10;
+        sum=sum*rem*rem*rem;
+        num/=10;
+
+    }
+    if (temp==sum)
+    {
+        printf("number is armstrong");
+    }
+    else{
+        printf("number is not armstrong");
+    }
+    return 0;
+}
