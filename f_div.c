c3f58e7371f76c53b0b245440d29f6e2cf45747c
sanjay <sanjaylashkarmaa1baba1@gmail.com>
2023-07-26 21:07:34 +0530

    function in c

---
+++ f_div.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int div(int a, int b)
+{
+
+    return a / b ;
+}
+int main()
+{
+    //  int a=5;
+    // int b=5;
+    int c;
+    c = div(25, 5);
+    printf(" the div is %d\n", c);
+    return 0;
+}
